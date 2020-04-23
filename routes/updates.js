7bb50afea1a51a9cf34428e81abda7f85264617c
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-04-23 14:41:13 -0500

    Update notify endpoint to receive download url

--- routes/updates.js
+++ routes/updates.js
@@ -7,7 +7,7 @@ const router = express.Router();
 const baseUrl = 'https://api.github.com';
 
 const notifyUpdate = (req, res) => {
-  const { repo, tag, token } = req.body;
+  const { repo, tag, token, apk } = req.body;
   if (!repo || !tag) return res.status(500).send({ error: 'Missing parameters' });
   if (!token || token.toString() !== process.env.NOTIFIER_KEY) {
     return res.status(500)
@@ -18,12 +18,13 @@ const notifyUpdate = (req, res) => {
       const { cache: channels = [] } = req.discordClient.channels;
       const channel = channels.find((it) => it.name.toLowerCase() === repo.toLowerCase());
       if (channel) {
+        const downloadUrl = apk.toString() || `https://jahir.dev/gh-releases/${repo.toLowerCase()}`;
         let messageBody = `**Changes:**\n${response.data.body || ''}`;
         messageBody += '\n\n**Useful links:**';
         messageBody += '\n* [How to update?](https://github.com/jahirfiquitiva/';
         messageBody += `${titleCase(repo)}/wiki/How-to-update)`;
-        messageBody += '\n* [Download sample APK](https://jahir.dev/gh-releases/';
-        messageBody += repo.toLowerCase();
+        messageBody += '\n* [Download sample APK](';
+        messageBody += downloadUrl;
         messageBody += ')\n* [Donate & support future development](https://jahir.dev/donate)';
         channel.send({
           embed: {

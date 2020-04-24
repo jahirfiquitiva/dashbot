e25f93858af26e944c7efb1d96a07a82e1078c7a
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-04-24 10:05:45 -0500

    Remove discord instance for web app

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const bot = require('./discord');
 
 const statusRoutes = require('./routes/status');
 const updatesRoutes = require('./routes/updates');
@@ -13,11 +12,6 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use((req, res, next) => {
-  req.discordClient = bot;
-  next();
-});
 
 app.use('/api', statusRoutes);
 app.use('/api/updates', updatesRoutes);

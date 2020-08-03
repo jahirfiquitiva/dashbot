40a49633f493f3f1d790ac8e6f4b176e729a736f
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-08-03 11:40:41 -0500

    Uninstall unused dependencies and make some messages more friendly

--- discord.js
+++ discord.js
@@ -1,28 +1,11 @@
 /* eslint-disable no-console */
 require('dotenv').config();
-const scheduler = require('node-schedule');
-const got = require('got');
 const Discord = require('discord.js');
 const { handleCommands, handleSimpleMessage } = require('./functions');
 
-const { REMINDERS_HOOK: remindersHook = '' } = process.env;
-
-const everySevenDays = '0 0 * * 0';
-// const everyThreeMinutes = '*/3 * * * *';
-const logDivider = '=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=';
-
-const reminderMessage = 'Here\'s just a friendly reminder that if you\'ve found the dashboards '
-  + 'or the [setup website](https://dashbud.dev) useful, '
-  + 'you can support my work by donating via:\n\n'
-  + '* [GitHub Sponsors](https://github.com/sponsors/jahirfiquitiva) (monthly)\n'
-  + '* [Buy me a pizza](https://buymeacoff.ee/jahirfiquitiva) (one-time or monthly)\n'
-  + '* [PayPal](https://jahir.xyz/DonatePayPal) (one-time)\n\n'
-  + 'Thanks in advance! :hugging:\n\n'
-  + '> *Don\'t forget to request the `supporter` role in <#704341896654225478> after you donate*\n'
-  + '> *Also, keep in mind GitHub sponsors get some rewards!*  :wink:';
-
 const client = new Discord.Client();
 
+// Quick validation of whether a message was sent by Jahir or not
 const jahirSentMessage = (actualAuthor, author) => {
   // TODO: Enable if testing
   // return false;
@@ -41,11 +24,15 @@ client.on('message', async (message) => {
     return;
   }
   const actualAuthor = author.id || '';
+  // Do not reply to other bots, if the message was deleted, or user is Jahir
   if (deleted || author.bot
     || actualAuthor.toString() === (process.env.BOT_USER_ID || '').toString()
     || jahirSentMessage(actualAuthor, author)) {
     return;
   }
+
+  // If message is a command handle it, otherwise check if message includes some keywords
+  // and reply accordingly
   if (text.startsWith(process.env.BOT_COMMAND_KEY)) {
     await handleCommands(message).catch((err) => console.error(err));
   } else {
@@ -53,6 +40,7 @@ client.on('message', async (message) => {
   }
 });
 
+// Initialize discord client
 try {
   client.login(process.env.BOT_TOKEN || '')
     .catch(() => {
@@ -60,41 +48,5 @@ try {
     });
 } catch (e) {
 }
-
-const sendWeeklyReminder = async () => {
-  if (!remindersHook || remindersHook.length <= 0) return;
-  try {
-    // noinspection JSCheckFunctionSignatures
-    await got.post(remindersHook, {
-      json: {
-        embeds: [{
-          title: ':wave:  Hello everyone! :grinning: ',
-          description: reminderMessage,
-          color: 15844367,
-        }],
-      },
-      responseType: 'json',
-    });
-    console.log(`Reminder sent @ ${new Date().toISOString()}`);
-  } catch (e) {
-    console.log('Error sending reminder');
-    console.error(e);
-  }
-};
-
-/*
-sendWeeklyReminder()
-  .then(() => {
-    const job = scheduler.scheduleJob(everySevenDays, (fireDate) => {
-      console.log(logDivider);
-      console.log(`This job was supposed to run @ ${fireDate.toISOString()}`);
-      sendWeeklyReminder()
-        .then(() => { console.log(logDivider); })
-        .catch((e) => { console.error((e)); });
-    });
-    console.log(`Reminders job scheduled? ${!!job}`);
-  })
-  .catch((e) => { console.error(e); });
-*/
 
 module.exports = client;
---
+++ reminder.js
@@ -0,0 +1,25 @@
+/* eslint-disable max-len */
+// This file content is commented on purpose, I just needed to keep a reference to this as I use it
+// in a cron job
+
+/*
+const reminderMessage = 'Here\'s just a friendly reminder that if you\'ve found the dashboards '
+  + 'or the [setup website](https://dashbud.dev) useful, '
+  + 'you can support my work by donating via:\n\n'
+  + '* [GitHub Sponsors](https://github.com/sponsors/jahirfiquitiva) (monthly)\n'
+  + '* [Buy me a pizza](https://buymeacoff.ee/jahirfiquitiva) (one-time or monthly)\n'
+  + '* [PayPal](https://jahir.xyz/DonatePayPal) (one-time)\n\n'
+  + 'Thanks in advance! :hugging:\n\n'
+  + '> *Don\'t forget to request the `supporter` role in <#704341896654225478> after you donate*\n'
+  + '> *Also, keep in mind GitHub sponsors get some rewards!*  :wink:';
+
+const reminderEmbed = {
+  embeds: [{
+    title: ':wave:  Hello everyone!  :grinning:',
+    description: reminderMessage,
+    color: 15844367,
+  }],
+};
+
+console.log(JSON.stringify(reminderEmbed, null, 2));
+ */
